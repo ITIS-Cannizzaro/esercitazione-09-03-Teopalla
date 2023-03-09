edefbfb7232dc0aed32521cbb29cab07a79833cd
utente <utente@DESKTOP-SUP9S26>
2023-03-09 10:19:09 +0100

    finito+commento

--- src/Pallavicini.java
+++ src/Pallavicini.java
@@ -43,17 +43,21 @@ public class Pallavicini
 		double [] array2 = new double [3];
 		int k=0;
 		int h=1;
+		//dichiaro variabili e array, k e h mi serviranno per indicare la posizione del secondo array
 		for(int i=0; i<array1.length; i++)
 			{
 				System.out.println("inserisci 6 valori" + i);
 				array1[i] =in.nextDouble();
 			}
+		//faccio il ciclo per dare 6 valori al primo array
 		for(int j=0; j<array2.length; j++)
 			{
 			array2[j]=(array1[k]*array1[h]);
 			k+=2;
 			h+=2;
 			System.out.println(array2[j]);
+		//con questo ciclo faccio avazare la posizione di array2 e all'interno di esso
+		//eseguo la moltiplicazione e incremento di 2 h e k in modo da passare alla prossima coppia da moltiplicare
 			}
 	}
 
